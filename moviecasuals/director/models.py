87e9d3d5327ddf646a6251a198f175a10ab09921
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-05 14:45:23 +0200

    Add director and movie models

--- moviecasuals/director/models.py
+++ moviecasuals/director/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from moviecasuals.accounts.models import MovieUserModel
+
+
+class Director(models.Model):
+    first_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+
+    last_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+
+    date_of_birth = models.DateField(
+        blank=False,
+        null=False,
+    )
+
+    biography = models.TextField()
+
+    picture_url = models.URLField(
+        blank=True,
+        null=True,
+    )
+
+    user = models.ForeignKey(
+        to=MovieUserModel,
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
--- moviecasuals/movie/models.py
+++ moviecasuals/movie/models.py
@@ -1,3 +1,90 @@
+from django.core.validators import MaxLengthValidator
 from django.db import models
+from django.db.models import Avg
 
-# Create your models here.
+from moviecasuals.accounts.models import MovieUserModel
+from moviecasuals.director.models import Director
+
+
+class Movie(models.Model):
+    user = models.ForeignKey(
+        to=MovieUserModel,
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE,
+    )
+
+    director = models.ForeignKey(
+        to=Director,
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE,
+    )
+
+    picture_url = models.URLField(
+        blank=False,
+        null=False,
+    )
+
+    description = models.TextField(
+        validators=[MaxLengthValidator(2000)],
+        help_text="Description should not exceed 2000 characters."
+    )
+
+    title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        unique=True
+    )
+    approved = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.title
+
+    def average_rating(self):
+        return self.ratings.aggregate(Avg('rating'))['rating__avg'] or 0
+
+
+class Comment(models.Model):
+    movie = models.ForeignKey(
+        to=Movie,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    user = models.ForeignKey(
+        to=MovieUserModel,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
+
+    text = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {str(self.movie)}"
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name='ratings'
+    )
+
+    user = models.ForeignKey(
+        to=MovieUserModel,
+        on_delete=models.CASCADE
+    )
+
+    rating = models.PositiveIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+
+    class Meta:
+        unique_together = ('movie', 'user')
+
+
