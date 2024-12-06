b0725a447f6da5a3c2e47cdbe9585abfa7172a84
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-06 16:32:03 +0200

    add view movie by genre and url

--- moviecasuals/movie/urls.py
+++ moviecasuals/movie/urls.py
@@ -4,4 +4,5 @@ from moviecasuals.movie import views
 
 urlpatterns = [
     path('add-movie/', views.add_movie, name='add-movie'),
+    path('movies-by-genre/<str:genre_choices>/', views.MovieByGenreView.as_view(), name='movies-by-genre'),
 ]
--- moviecasuals/movie/views.py
+++ moviecasuals/movie/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from moviecasuals.movie.models import Movie
+
 
 def add_movie(request):
     return render(request, 'movie/add_movie.html')
+
+
+def movie_by_genre(request):
+    return render(request, 'movie/movies_by_genre.html')
+
+
+class MovieByGenreView(ListView):
+    template_name = 'movie/movies_by_genre.html'
+    context_object_name = 'movies'
+
+    def get_queryset(self):
+        genre_choices = self.kwargs['genre_choices']
+        return Movie.objects.filter(genre_choices=genre_choices)
