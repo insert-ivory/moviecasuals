a67943b19ee3446805e0429d02716a7310ba6367
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-07 14:44:04 +0200

    fix url in create director

--- moviecasuals/director/urls.py
+++ moviecasuals/director/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from moviecasuals.director import views
 
 urlpatterns = [
-    path('add-director/', views.MovieUserCreateDirectorView.as_view(), name='add-director'),
+    path('create-director/', views.MovieUserCreateDirectorView.as_view(), name='create-director'),
     path('<int:director_id>/director-details', views.DirectorDetails.as_view(), name='director-details'),
     path('<int:director_id>/edit-director', views.EditDirectorView.as_view(), name='edit-director'),
     path('<int:director_id>/delete-director', views.DeleteDirectorView.as_view(), name='delete-director'),
