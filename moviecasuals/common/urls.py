9ebb359f71c583b47e0f92742288588efb5da1b3
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-07 18:57:33 +0200

    updated urls for comments

--- moviecasuals/common/urls.py
+++ moviecasuals/common/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 
-from moviecasuals.common.views import HomePageView, AddCommentView, EditCommentView, DeleteCommentView
+from moviecasuals.common.views import HomePageView, AddCommentView, EditCommentView, DeleteCommentView, \
+    AccessControlView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
-    path('add_comment/<int:movie_id>/', AddCommentView.as_view(), name='add_comment'),
     path('<int:pk>/', include([
+        path('add_comment/', AddCommentView.as_view(), name='add_comment'),
         path('edit_comment/', EditCommentView.as_view(), name='edit_comment'),
         path('delete_comment/', DeleteCommentView.as_view(), name='delete_comment'),
     ])),
+    # path('add_comment/', AddCommentView.as_view(), name='add_comment'),
+    # path('<int:pk>/', include([
+    #     path('edit_comment/', EditCommentView.as_view(), name='edit_comment'),
+    #     path('delete_comment/', DeleteCommentView.as_view(), name='delete_comment'),
+    path('access-control/', AccessControlView.as_view(), name='access-control'),
 ]
