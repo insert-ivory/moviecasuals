481b4ff65f84e2da9a5ac693949498930c7c7f73
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-07 17:27:16 +0200

    added movie details url

--- moviecasuals/movie/urls.py
+++ moviecasuals/movie/urls.py
@@ -5,4 +5,5 @@ from moviecasuals.movie import views
 urlpatterns = [
     path('cereate-movie/', views.CreateMovieView.as_view(), name='create-movie'),
     path('movies-by-genre/<str:genre_choices>/', views.MovieByGenreView.as_view(), name='movies-by-genre'),
+    path('<int:id>/movie-details/', views.MovieDetailsView.as_view(), name='movie-details'),
 ]
