8ea99fa6a41ed1e43e359a952f95b2c9804e7160
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-07 22:49:37 +0200

    add:path for searchbar url

--- moviecasuals/common/urls.py
+++ moviecasuals/common/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from moviecasuals.common.views import HomePageView, AddCommentView, EditCommentView, DeleteCommentView, \
-    AccessControlView
+    AccessControlView, SearchBarView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
@@ -15,4 +15,5 @@ urlpatterns = [
     #     path('edit_comment/', EditCommentView.as_view(), name='edit_comment'),
     #     path('delete_comment/', DeleteCommentView.as_view(), name='delete_comment'),
     path('access-control/', AccessControlView.as_view(), name='access-control'),
+    path('searchbar/', SearchBarView.as_view(), name='searchbar'),
 ]
