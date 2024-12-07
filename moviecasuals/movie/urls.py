cfc6b1bbd678bf930007afad1ab95e72d8e4d273
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-07 16:23:49 +0200

    Add new path create movie

--- moviecasuals/movie/urls.py
+++ moviecasuals/movie/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from moviecasuals.movie import views
 
 urlpatterns = [
-    path('add-movie/', views.add_movie, name='add-movie'),
+    path('cereate-movie/', views.CreateMovieView.as_view(), name='create-movie'),
     path('movies-by-genre/<str:genre_choices>/', views.MovieByGenreView.as_view(), name='movies-by-genre'),
 ]
