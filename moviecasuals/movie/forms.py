0b41358edb37ff75cf1e1b5ac62909f9379ce77b
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-07 16:20:20 +0200

    add: createmovie form

--- moviecasuals/movie/forms.py
+++ moviecasuals/movie/forms.py
@@ -1,16 +1,51 @@
 from django import forms
 
+from moviecasuals.director.models import Director
 from moviecasuals.movie.models import Movie, Comment
 
 
 class BaseMovieForm(forms.ModelForm):
     class Meta:
         model = Movie
-        exclude = ['approved']
+        exclude = ['approved', 'user']
 
 
 class CreateMovieForm(BaseMovieForm):
-    pass
+    director = forms.CharField(
+        label="Director",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': "Write the Director's full name"
+        }),
+        error_messages={
+            'required': "Please make sure to provide the director's full name."
+        }
+    )
+
+    class Meta(BaseMovieForm.Meta):
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Movie's Title..."}),
+            'genre_choices': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 3, 'placeholder': "Movie's description..."}),
+            'picture_url': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Image url...'}),
+        }
+
+    def clean_director(self):
+        director_name = self.cleaned_data.get('director')
+        try:
+            first_name, last_name = director_name.split(' ', 1)
+            director = Director.objects.get(first_name=first_name, last_name=last_name)
+        except (ValueError, Director.DoesNotExist):
+            raise forms.ValidationError("Both names should match an already existing director.")
+        return director
+
+    def save(self, commit=True):
+        movie = super().save(commit=False)
+        movie.director = self.cleaned_data['director']
+        if commit:
+            movie.save()
+        return movie
 
 
 class BaseCommentForm(forms.ModelForm):
