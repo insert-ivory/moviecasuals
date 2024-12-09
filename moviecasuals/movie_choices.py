b85b9bac636768f17442ae12488be5ff0bbf9c5a
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-09 16:20:35 +0200

    add: new choices class for movie options

--- moviecasuals/movie_choices.py
+++ moviecasuals/movie_choices.py
@@ -17,3 +17,14 @@ class MovieChoices(models.TextChoices):
     MUSICAL = 'Musical', 'Musical'
     CRIME = 'Crime', 'Crime'
     OTHERS = 'Other Genres', 'Other Genres'
+
+
+class MovieUserOptions(models.TextChoices):
+    UNSPECIFIED = "Unspecified", "Unspecified"
+    WANT_TO_WATCH = "Want to Watch", "Want to Watch"
+    WATCHED = "Watched", "Watched"
+    NEED_TO_REWATCH = "Need to Rewatch", "Need to Rewatch"
+    INTERESTED = "Interested", "Interested"
+    NOT_INTERESTED = "Not Interested", "Not Interested"
+    RECOMMENDED = "Recommended", "Recommended"
+
