ea368338b85ef76c5e2d061372b1b9564b471868
Ivaylo Kovachev <ivailo550@gmail.com>
2024-12-11 15:15:25 +0200

    update: accesscontrolmixin fixed permission for groups

--- moviecasuals/mixins.py
+++ moviecasuals/mixins.py
@@ -9,19 +9,40 @@ class AccessControlMixin:
         # Default to 'user', but allow subclasses to specify their own user attribute
         return 'user'
 
+    def has_permission(self, user, obj, user_attribute):
+        """
+        Check if the user has permission to access the given object.
+        Redactors (staff but not admin) are restricted from editing accounts and comments.
+        """
+        if user.is_superuser:
+            return True  # Superusers can access everything
+
+        if user.is_staff:
+            # Check if the user is a redactor or admin
+            if 'Content Approver' in [group.name for group in user.groups.all()]:
+                # Redactors can only access movie-related objects
+                if user_attribute == 'movie' or user_attribute == 'director':
+                    return True
+                return False  # Redactors can't access accounts or comments
+
+            if 'Admins' in [group.name for group in user.groups.all()]:
+                # Admins can access everything
+                return True
+
+        # If it's neither a redactor nor admin, allow access based on user attribute
+        if user_attribute == 'self':
+            return obj == user  # Only allow access to their own data
+
+        return getattr(obj, user_attribute) == user  # Allow if the object belongs to the user
+
     def get(self, request, *args, **kwargs):
         obj = self.get_object()
         user_attribute = self.get_user_attribute()
         user = request.user
 
-        if user_attribute == 'self':
-            if not (obj == user or user.is_superuser or user.is_staff):
-                messages.error(request, self.DENIED_MESSAGE)
-                return redirect('access-control')
-        else:
-            if not (getattr(obj, user_attribute) == user or user.is_superuser or user.is_staff):
-                messages.error(request, self.DENIED_MESSAGE)
-                return redirect('access-control')
+        if not self.has_permission(user, obj, user_attribute):
+            messages.error(request, self.DENIED_MESSAGE)
+            return redirect('access-control')
 
         return super().get(request, *args, **kwargs)
 
@@ -30,14 +51,9 @@ class AccessControlMixin:
         user_attribute = self.get_user_attribute()
         user = request.user
 
-        if user_attribute == 'self':
-            if not (obj == user or user.is_superuser or user.is_staff):
-                messages.error(request, self.DENIED_MESSAGE)
-                return redirect('access-control')
-        else:
-            if not (getattr(obj, user_attribute) == user or user.is_superuser or user.is_staff):
-                messages.error(request, self.DENIED_MESSAGE)
-                return redirect('access-control')
+        if not self.has_permission(user, obj, user_attribute):
+            messages.error(request, self.DENIED_MESSAGE)
+            return redirect('access-control')
 
         return super().post(request, *args, **kwargs)
 
